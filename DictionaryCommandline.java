14a6861ba94bdd2ccdf22d52cb9b8e456c16ce5a
tuananhhm0211 <tuananhhmx4@gmail.com>
2021-10-08 10:09:27 +0700

    add ham dictionaryBasic phien ban 1

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,10 +1,29 @@
 public class DictionaryCommandline {
+    /** show all word.
+     *
+     */
     public static void showAllWord() {
         int dem = 1;
-        System.out.println("No" + "     " + "|English" + "      |Vietnamese");
+        System.out.println("No" + "         " + "|English" + "          |Vietnamese");
         for (Word word : Dictionary.list) {
-            System.out.println(dem + "      |" + word.getWord_target() + "       |" + word.getWord_explain());
+            System.out.println(dem + "          |" + word.getWord_target() + "              |" + word.getWord_explain());
             dem++;
         }
+    }
+
+    /** dictionaryBasic.
+     *
+     */
+    public static void dictionaryBasic() {
+        DictionaryManagement.insertFromCommandline();
+        showAllWord();
+    }
+
+    /** Test.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        dictionaryBasic();
     }
 }
